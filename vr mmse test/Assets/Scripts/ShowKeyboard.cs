71253afc67e672cb22299a23b04be8260daeb8ce
rrrrrr0958 <bee19623@gmail.com>
2025-08-20 17:14:27 +0800

    登入虛擬鍵盤做好了
    接著想要做使用者註冊時的基本資料點選 ex.大頭貼選擇 年齡輸入等等

---
+++ vr mmse test/Assets/Scripts/ShowKeyboard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Microsoft.MixedReality.Toolkit.Experimental.UI;
+
+public class ShowKeyboard : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private TMP_InputField inputField;
+    void Start()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        inputField.onSelect.AddListener(x => OpenKeyboard());
+    }
+
+    // Update is called once per frame
+    public void OpenKeyboard()
+    {
+        NonNativeKeyboard.Instance.InputField = inputField;
+        NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
+    }
+}
