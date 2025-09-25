a34ba94ce6d6ce5fe1ab6b7c1f9596bd87ba1219
alanchang張耀仁 <zy84946@gmail.com>
2025-09-25 21:14:17 +0800

    感謝黃大姊辛苦修復

--- vr mmse test/Assets/Scripts/RecognitionResponse_3_4_5.cs
+++ vr mmse test/Assets/Scripts/RecognitionResponse_3_4_5.cs
@@ -1,7 +1,10 @@
 using System;
+using UnityEngine; // 確保有這個，如果你在 Unity 裡使用 JsonUtility
 
 [System.Serializable]
 public class RecognitionResponse
 {
     public string transcription;
+    // 為了涵蓋 Flask 可能返回的錯誤，最好也加上 error 欄位
+    public string error; 
 }
