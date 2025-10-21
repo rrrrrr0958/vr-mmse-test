9167e76fbe14972293dd6e92134c3df114f58850
rrrrrr0958 <bee19623@gmail.com>
2025-10-22 00:44:27 +0800

    結束遊戲按鈕做完 可合併

--- vr mmse test/Assets/Scripts/TestHistoryUI.cs
+++ vr mmse test/Assets/Scripts/TestHistoryUI.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 using TMPro;
 using Firebase.Firestore;
 using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
 
 public class TestHistoryUI : MonoBehaviour
 {
     private FirebaseManager_Firestore firebaseManager;
     [SerializeField] private Transform listParent;
     [SerializeField] private GameObject itemPrefab;
+    public Button endGameButton; 
+
+    public AudioSource audioSource;
+    public AudioClip confirmSound;
+    [Range(0f, 1f)] public float confirmVolume = 1f;
 
     void Start()
     {
@@ -56,5 +63,24 @@ public class TestHistoryUI : MonoBehaviour
             Debug.Log($"#{index} | 開始時間: {startTimestamp} | 分數: {totalScore} | 花費時間: {totalTime}");
             index++;
         }
+    }
+
+    public void OnContinueButtonClicked()
+    {
+        StartCoroutine(HandleContinueButton());
+    }
+
+    private IEnumerator HandleContinueButton()
+    {
+        if (confirmSound != null && audioSource != null)
+            audioSource.PlayOneShot(confirmSound, confirmVolume);
+
+        yield return new WaitForSeconds(0.5f);
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;  // 在 Editor 中停止播放
+#else
+        Application.Quit();  // 在 Build 後結束遊戲
+#endif
     }
 }
